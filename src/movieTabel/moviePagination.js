d2ed33549dc5ed5a62fb448e2ef99f31086b6d8d
Faiz-Kfueit <faizpasha525@gmail.com>
2022-08-21 22:45:35 -0700

    first comment

---
+++ src/movieTabel/moviePagination.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Pagination as PRB } from "react-bootstrap";
+
+function Pagination({
+  totalMovies,
+  moviesPerPage,
+  currentPage,
+  handlePaginate,
+}) {
+  const pages = new Array(Math.ceil(totalMovies / moviesPerPage)).fill(0);
+  return (
+    <PRB>
+      {pages.map((_value, index) => {
+        const number = index + 1;
+        <PRB.Item
+          key={number}
+          active={number === currentPage}
+          onClick={() => handlePaginate(number)}
+        >
+          {number}
+        </PRB.Item>;
+      })}
+    </PRB>
+  );
+}
+
+export default Pagination;
